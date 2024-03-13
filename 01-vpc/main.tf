785761c174ce922e030971bebd93d2c704512c3c
mutthuappalaraju <arappalaraju@gmail.com>
2024-03-13 17:08:07 +0530

    terraform roboshop

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,6 +1,6 @@
 module "roboshop" {
   #source = "../terraform-aws-vpc"
-  source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
+  source = "git::https://github.com/mutthuappalaraju/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
@@ -15,4 +15,8 @@ module "roboshop" {
   # database subnet
   database_subnets_cidr = var.database_subnets_cidr
 
+  #peering
+  is_peering_required = var.is_peering_required
+
 }
+
